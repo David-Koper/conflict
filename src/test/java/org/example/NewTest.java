c03142ae6c3e98b7133631425d216221fee381b9
David Kuznetsov <kuznetsov.david87@gmail.com>
2022-05-29 13:26:19 +0300

    First Commit

---
+++ src/test/java/org/example/NewTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class NewTest {
+    @Test
+    void NewTest (){
+    Assertions.assertTrue(3>1);}
+}
